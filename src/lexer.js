45397e57102d739214a1c0a33fe952df14ecc22e
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-07 21:45:41 -0400

    add string definition to lexer

--- src/lexer.js
+++ src/lexer.js
@@ -4,6 +4,14 @@ let lexer = moo.compile
 (
     {
         WS:     /[ \t]*/,
+
+        // PARENS
+        LPAREN: "(",
+        RPAREN: ")",
+
+        // STRINGS
+        string: /"(?:\\["\\]|[^\n"\\])*"/
+        
         NL: {match: /\n/, lineBreaks: true}
     }
 )
