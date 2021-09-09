814a1c079883351bb3beeab962c7a07b6337418d
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-08 21:36:05 -0400

    finished token constructors

--- src/token/token_constructor.js
+++ src/token/token_constructor.js
@@ -13,3 +13,51 @@ class Token
         else this.value=null
     }
 }
+
+class TokenList
+{
+    /**
+     * @private
+     */
+    tokens = [];
+
+    /**
+     * @private
+     */
+    tokensRaw=[];
+
+    constructor()
+    {
+        this.tokens=[]
+        this.tokensRaw=[]
+    }
+
+    /**
+     * @param {Token} token
+     */
+    add(token)
+    {
+        this.tokens.push(token)
+        if(token.value) this.tokensRaw.push(`${token.type}:${token.value}`)
+        else this.tokensRaw.push(token.type)
+    }
+
+    /**
+     * @param {Number} index
+     */
+    remove(index)
+    {
+        this.tokens.splice(index,1)
+        this.tokensRaw.splice(index,1)
+    }
+
+    listTokens()
+    {
+        return this.tokens
+    }
+
+    list()
+    {
+        return this.tokensRaw
+    }
+}
