97a3d9055629b51679857493091c67629643b916
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-09-09 23:14:38 -0400

    Create dunamis-0.0.0.1-setup.sh

---
+++ installer/linux/dunamis-0.0.0.1-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt install
+sudo apt install git 
+sudo apt install build-essential
+
+clear
+cd /usr/local/
+git clone https://github.com/TechPenguineer/Dunamis.git
+cd Dunamis 
+make install
